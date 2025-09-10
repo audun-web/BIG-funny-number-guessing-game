d7f8ff0be77b62988cfe63d6747e634cd41caf2d
84mecaud <audun@midtmeck.com>
2025-09-10 14:16:43 +0200

    audun_test123

--- higher lower game.kt
+++ higher lower game.kt
@@ -1,44 +1,39 @@
 /*
    |---------------------------|
-   | Made by Audun and chatgpt |
+   | Made by Audun and ChatGPT |
    |---------------------------|
  */
 
-var gamesplayed = 0
+var gamesPlayed = 0L
 
 fun main() {
     var playAgain = true
 
     while (playAgain) {
-        print("\u001B[1m\u001B[36mWhat gamemode do you want? easy, medium, hard or extreme.: \u001B[0m")
+        print("\u001B[1m\u001B[36mWhat gamemode do you want? Easy, Medium, Hard, Extreme or Impossible: \u001B[0m")
         val gamemode = readlnOrNull()?.lowercase()
 
-        val range = when (gamemode) {
-            "easy", "e" -> 1..100
-            "medium", "m" -> 1..250
-            "hard", "h" -> 1..1000
-            "extreme" -> 1..100000
-            "imposable" -> 1..1000000000
+        val range: LongRange = when (gamemode) {
+            "easy", "e" -> 1L..100L
+            "medium", "m" -> 1L..250L
+            "hard", "h" -> 1L..1000L
+            "extreme" -> 1L..100_000L
+            "impossible" -> 1L..Long.MAX_VALUE // future-proof
             else -> {
                 println("Invalid choice, defaulting to Easy.")
-                1..100
+                1L..100L
             }
         }
 
         val number = range.random()
-        var attempts = 0
+        var attempts = 0L
         var guessed = false
 
         println("\u001B[1m\u001B[33mI picked a number between ${range.first} and ${range.last}. Can you guess it?\u001B[0m")
 
-        /* Dev */
-        // println(number)
-
-
         while (!guessed) {
             print("\u001B[1m\u001B[36mYour guess: \u001B[0m")
-            val guess = readlnOrNull()?.toIntOrNull()
-            Runtime.getRuntime().exec(arrayOf("afplay", "/System/Library/Sounds/Glass.aiff"))
+            val guess = readlnOrNull()?.toLongOrNull() // <-- changed to Long
 
             if (guess == null) {
                 println("\u001B[1m\u001B[31mPlease enter a valid number!\u001B[0m")
@@ -52,10 +47,9 @@ fun main() {
 
             attempts++
 
-            if (attempts % 10 == 0) {
-                println("\u001B[32mYou have used $attempts attempts now")
+            if (attempts % 10 == 0L) {
+                println("\u001B[32mYou have used $attempts attempts now\u001B[0m")
             }
-
 
             when {
                 guess < number -> println("\u001B[1m\u001B[34mToo low!\u001B[0m")
@@ -63,7 +57,7 @@ fun main() {
                 else -> {
                     println("\u001B[1m\u001B[32mCorrect! The number was $number. You guessed it in $attempts tries.\u001B[0m")
                     guessed = true
-                    gamesplayed++
+                    gamesPlayed++
                 }
             }
         }
@@ -73,9 +67,6 @@ fun main() {
         playAgain = response == "y"
     }
 
-    if (gamesplayed < 2) {
-        println("\u001B[1m\u001B[32mYou´ve played $gamesplayed game! Thanks for playing! Goodbye!\u001B[0m")
-    } else {
-        println("\u001B[1m\u001B[32mYou´ve played $gamesplayed games! Thanks for playing! Goodbye!\u001B[0m")
-    }
+    val gameText = if (gamesPlayed == 1L) "game" else "games"
+    println("\u001B[1m\u001B[32mYou've played $gamesPlayed $gameText! Thanks for playing! Goodbye!\u001B[0m")
 }
